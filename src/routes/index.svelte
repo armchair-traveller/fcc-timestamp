8ed4c28ad7406f186a49d359072143bf3a9e76e7
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-08-02 23:56:44 -0700

    fix links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script>
-  var origin =
-    typeof window != "undefined" ? window.location.origin : "[ORIGIN URL]";
+  const apiOrigin = "alpha.fangdev.workers.dev/fcc/ts-ms";
 </script>
 
 <div
@@ -9,16 +8,21 @@
   <section class="w-10/12 max-w-prose">
     <h1 class="font-extrabold text-2xl">Timestamp Microservice</h1>
     <p class="ml-4">
-      by <span class="text-[#FFE193] font-medium">Fang🦁</span>
+      by <span
+        class="text-[#FFE193] font-medium hover:font-bold focus:font-bold"
+        ><a href="https://github.com/armchair-traveller">Fang</a>🦁</span
+      >
     </p>
     <div class="mt-3 font-medium">
       <p>Example Usage</p>
       <div class="text-[#A1B8F3] ml-4">
-        <a href="{origin}/api/2015-12-25" class="hover:font-bold"
-          >{origin}/api/2015-12-25</a
+        <a
+          href="https://{apiOrigin}/2015-12-25"
+          class="hover:font-bold focus:font-bold">{apiOrigin}/2015-12-25</a
         >
-        <a href="{origin}/api/1451001600000" class="hover:font-bold"
-          >{origin}/api/1451001600000</a
+        <a
+          href="https://{apiOrigin}/1451001600000"
+          class="hover:font-bold focus:font-bold">{apiOrigin}/1451001600000</a
         >
       </div>
     </div>
